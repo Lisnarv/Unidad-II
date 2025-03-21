96746bf4299d4364c8f67e0ffac184acd829c5e3
lisnarv <valeralisnar30@gmail.com>
2025-03-21 13:50:51 +0100

    estableciendo rutas

--- app.js
+++ app.js
@@ -3,24 +3,36 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose'); 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var itemsRouter = require('./routes/items'); 
 
 var app = express();
 
+mongoose.connect('mongodb://localhost:27017/mi_basedatos', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Conectado a MongoDB'))
+.catch(err => console.error('Error al conectar a MongoDB:', err));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
+// Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/items', itemsRouter); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
